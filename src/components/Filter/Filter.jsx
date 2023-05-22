3f41aa680fc2e2169cf25f20c06310687069ba27
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-05-22 10:37:48 +0200

    reverst classes to hooks

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import css from './Filter.module.css';
-
-// const Filter = ({ value, onChange }) => (
-//   <input
-//     type="text"
-//     placeholder="Search contacts"
-//     value={value}
-//     onChange={onChange}
-//     className={css.input}
-//   />
-// );
-
-// Filter.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   onChange: PropTypes.func.isRequired,
-// };
-
-// export default Filter;
 import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Filter.module.css';
